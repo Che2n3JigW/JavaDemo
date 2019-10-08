515418e5a2b554cd41f0d581f924a0cf15b2ce06
jChenys <313629058@qq.com>
2019-10-08 18:14:56 +0800

    StringBuffer的使用

---
+++ src/string/StringDemo.java
@@ -0,0 +1,16 @@
+package string;
+
+/**
+ * StringBuilder不安全,StringBuffer安全
+ * StringBuilder快,StringBuffer慢
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        StringBuffer sBuffer = new StringBuffer(" test");
+        sBuffer.append(" String Buffer");
+
+        sBuffer.reverse();
+
+        System.out.println(sBuffer);
+    }
+}
